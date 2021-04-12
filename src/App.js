3760426ddd5d300c4c516eea3feaf32f7254ce51
elias-barbero <elias.barbero@hotmail.com>
2021-04-12 20:23:12 +0200

    second  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Gantt, { Tasks, Dependencies, Resources, ResourceAssignments, Column, Editing, Toolbar, Item, Validation } from 'devextreme-react/gantt';
+
+import { tasks, dependencies, resources, resourceAssignments } from './data.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Gantt
+        taskListWidth={500}
+        scaleType="weeks"
+        height={700}>
+
+        <Tasks dataSource={tasks} />
+        <Dependencies dataSource={dependencies} />
+        <Resources dataSource={resources} />
+        <ResourceAssignments dataSource={resourceAssignments} />
+
+        <Toolbar>
+          <Item name="undo" />
+          <Item name="redo" />
+          <Item name="separator" />
+          <Item name="collapseAll" />
+          <Item name="expandAll" />
+          <Item name="separator" />
+          <Item name="addTask" />
+          <Item name="deleteTask" />
+          <Item name="separator" />
+          <Item name="zoomIn" />
+          <Item name="zoomOut" />
+        </Toolbar>
+
+        <Column dataField="title" caption="Subject" width={300} />
+        <Column dataField="start" caption="Start Date" />
+        <Column dataField="end" caption="End Date" />
+
+        <Validation autoUpdateParentTasks={true} />
+        <Editing enabled={true} />
+      </Gantt>
+    );
+  }
 }
 
 export default App;
